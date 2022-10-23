dabc76a93f5d2f33f32d7f4a6dd3bf97e245f311
MoosaZk <moosazkhan@gmail.com>
2022-10-24 00:14:12 +0500

    signUP project

--- weather_project/app.js
+++ weather_project/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const http = require("https"); 
 const app = express();
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}))
 const port = 3000;
 
 app.get("/", (req,res)=>{
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Karachi&units=metric&appid=c6b27262e3c19fdabf70c48b9cf3f465";
+    res.sendFile(__dirname + "/index.html")
+
+})
+
+app.post("/", (req,res) => {
+    const city = req.body.cityName;
+    const apiKey = "c6b27262e3c19fdabf70c48b9cf3f465"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=" + apiKey;
+    
     http.get(url,(resp) => {
         console.log(resp.statusCode);
 
@@ -16,14 +25,17 @@ app.get("/", (req,res)=>{
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
+            const imgUrl= "http://openweathermap.org/img/wn/" + icon + "@2x.png" 
+
             console.log("description: "+ description);
-            res.write("<h1> Degree in Karachi:  </h>" + temp +"");
-            res.write("\nDescription: " + description);
+            res.write("<h1> Degree in "+ city +":  " + temp +" </h1>");
+            res.write("<br> Description: " + description);
+            res.write("<img src=" + imgUrl +">")
             res.send();
 
         })
 
-    })
+     })
 
 })
 
